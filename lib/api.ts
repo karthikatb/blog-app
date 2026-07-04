1220ecadbc50cfc3f88432f66c92a2b1b24b09e0
karthikatb <karthikatb20@gmail.com>
2026-07-04 11:53:53 +0530

    Replace json-server with local data

--- lib/api.ts
+++ lib/api.ts
@@ -1,14 +1,10 @@
-import axios from "axios";
 import { Blog } from "@/types/blog";
-
-const API_URL = "http://localhost:3002/posts";
+import { posts } from "@/data/posts";
 
 export async function fetchPosts(): Promise<Blog[]> {
-  const response = await axios.get(API_URL);
-  return response.data;
+  return posts;
 }
 
-export async function fetchPost(id: string): Promise<Blog> {
-  const response = await axios.get(`${API_URL}/${id}`);
-  return response.data;
+export async function fetchPost(id: string): Promise<Blog | undefined> {
+  return posts.find((post) => post.id === id);
 }
